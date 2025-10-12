2971e3a9cd0fe093106af196498fbc77c20024c4
Dimitriev Denis <Deniska-1616@mail.ru>
2025-10-12 22:18:39 +0300

    fix

--- rebuild.sh
+++ rebuild.sh
@@ -7,7 +7,7 @@ npm run build:prod
 sudo rm -rf /var/www/dist
 
 # Переместить папку dist из директории проекта фронтенда в /var/www/
-sudo mv /opt/Affiliate_Program/frontend_v2.0/dist /var/www/
+sudo mv /opt/Affiliate_Program/AffiliateProgramFrontend_v2/dist /var/www/
 
 # Перезапустить сервис Nginx
 sudo service nginx restart
