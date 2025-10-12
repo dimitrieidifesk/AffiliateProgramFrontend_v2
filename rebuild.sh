f23931e3abbb21aeb5803ca43bad30a0475b4064
Dimitriev Denis <Deniska-1616@mail.ru>
2025-10-12 22:21:17 +0300

    fix

--- rebuild.sh
+++ rebuild.sh
@@ -6,6 +6,9 @@ npm run build
 # Удалить папку dist в /var/www/
 sudo rm -rf /var/www/dist
 
+# переименовать /build в /dist
+mv /opt/Affiliate_Program/AffiliateProgramFrontend_v2/build /opt/Affiliate_Program/AffiliateProgramFrontend_v2/dist
+
 # Переместить папку dist из директории проекта фронтенда в /var/www/
 sudo mv /opt/Affiliate_Program/AffiliateProgramFrontend_v2/dist /var/www/
 
